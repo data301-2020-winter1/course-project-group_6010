ac24a328817168244ef030e2d1a8274699a6ebac
Prostigus <prajeetdidden@gmail.com>
2020-11-10 15:38:46 -0800

    Pushed test of functions

---
+++ analysis/scripts/project_function.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def load_and_process(csv_file):
+    df1=(
+        pd.read_csv(csv_file)
+         
+         )
+    df2=(
+        df1
+        )
+    return df2
