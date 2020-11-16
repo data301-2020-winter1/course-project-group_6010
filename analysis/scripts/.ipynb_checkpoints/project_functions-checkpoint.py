8e3b60c89beb503ce0cd70624afbe713c0bd7692
Prostigus <prajeetdidden@gmail.com>
2020-11-15 17:16:34 -0800

    Cleaned and processed data

--- analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
+++ analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 
+def unprocessed(csv_file):
+    df = pd.read_csv(csv_file)
+    return df
+
 def load_and_process(csv_file):
-    df1=(
-        pd.read_csv(csv_file)
+    df = pd.read_csv(csv_file)
+    df1=(df.dropna(subset=["Year_of_Release","Publisher","Critic_Score","Critic_Count","User_Score","User_Count","Developer","Rating"]) 
+        .sort_values("Genre")
+        .reset_index(drop=True)
+        )    
+    return df1
          
-         )
-    df2=(
-        df1
-        )
-    return df2
